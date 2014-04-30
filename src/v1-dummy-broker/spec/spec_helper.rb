7f5e3f39d63c643947b766405d93a49267c63faa
Hristo Iliev and Zach Robinson <zrobinson@pivotallabs.com>
2014-04-30 14:19:22 -0700

    remove unused test setup

--- src/v1-dummy-broker/spec/spec_helper.rb
+++ src/v1-dummy-broker/spec/spec_helper.rb
@@ -3,9 +3,3 @@ BROKER_SPEC_ROOT = File.dirname(__FILE__)
 require File.expand_path('../../lib/dummy', __FILE__)
 require File.expand_path('../vendor/integration-test-support/support/integration_example_group.rb', File.dirname(__FILE__))
 require File.expand_path('../support/broker_runner', __FILE__)
-
-IntegrationExampleGroup.tmp_dir = '/tmp'
-
-RSpec.configure do |config|
-  config.include IntegrationExampleGroup, type: :integration, example_group: {file_path: /\/integration\//}
-end
